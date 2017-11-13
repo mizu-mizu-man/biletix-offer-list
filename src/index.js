b71fbd987619dfd8d54b0d97155c18d01dd6e1c8
Fanta Straus <mizu-mizu@yandex.ru>
2017-11-13 17:26:20 +0300

    Small, bun nasty typo fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
-import App from './components/app';
+import App from './components/App';
 import reducers from './reducers';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
